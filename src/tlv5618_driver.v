a74a163017912858ac54b69ef25ba3a79a2ef1a6
Cc-aw <455457721@qq.com>
2025-05-13 15:24:38 +0800

    second commit

--- src/tlv5618_driver.v
+++ src/tlv5618_driver.v
@@ -4,17 +4,218 @@ module tlv5618_driver (
     input rst_n,
 
     // to user
-    input  [15:0] DAC_data,
-    input         set_go,
-    output        set_done,
+    input      [15:0] set_data,
+    input             set_go,
+    output reg        set_done,
 
     //to DAC
-    output DAC_cs_n,
-    output DAC_sclk,
-    output DAC_din
+    output reg DAC_cs_n,
+    output reg DAC_sclk,
+    output reg DAC_din
 );
     parameter CLOCK_FREQ = 50_000_000;
-    parameter SRCLK_FREQ = 20_000_000;
-    parameter MCNT_DIV = CLOCK_FREQ / (SRCLK_FREQ *2 ) -1;
+    parameter SRCLK_FREQ = 12_500_000;
+    parameter MCNT_DIV = CLOCK_FREQ / (SRCLK_FREQ * 2) - 1;
+
+    //计数器
+    reg [ 7:0] div_cnt;
+    reg [ 5:0] seq_cnt;
+    reg        en_div_cnt;
+
+    //暂存数据 
+    reg        r_set_go;
+    reg [15:0] r_set_data;
+
+    //暂存set_go
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            r_set_go <= 0;
+        end
+        else begin
+            r_set_go <= set_go;
+        end
+    end
+
+    //暂存set_data
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            r_set_data <= 16'd0;
+        end
+        else begin
+            r_set_data <= set_data;
+        end
+    end
+
+    //分频计数器，实现12.5Mhz下的最小时间单位40ns
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            div_cnt <= 0;
+        end
+        else if (en_div_cnt) begin
+            if (div_cnt == MCNT_DIV) begin
+                div_cnt <= 0;
+            end
+            else begin
+                div_cnt <= div_cnt + 1;
+            end
+        end
+        else begin
+            div_cnt <= 0;
+        end
+    end
+
+    //序列计数器
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            seq_cnt <= 0;
+        end
+        else if (seq_cnt == 33) begin
+            seq_cnt <= 0;
+        end
+        else begin
+            seq_cnt <= seq_cnt + 1;
+        end
+    end
+
+    // en_div_cnt 生成信号
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            en_div_cnt <= 0;
+        end
+        else if (set_go) begin
+            en_div_cnt <= 1;
+        end
+        else if ((div_cnt == MCNT_DIV) && (seq_cnt == 33)) begin
+            en_div_cnt <= 0;
+        end
+        else begin
+            en_div_cnt <= en_div_cnt;
+        end
+    end
+
+    //set_done
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            set_done <= 0;
+        end
+        else if ((div_cnt == MCNT_DIV) && (seq_cnt == 33)) begin
+            set_done <= 1;
+        end
+        else begin
+            set_done <= 0;
+        end
+    end
+    //序列机实现
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            DAC_sclk <= 1;
+            DAC_din  <= 0;
+            DAC_cs_n <= 1;
+        end
+        else if (div_cnt == MCNT_DIV) begin
+            case (seq_cnt)
+                0: begin
+                    DAC_cs_n <= 0;
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[15];
+                end
+                1:  DAC_sclk <= 0;
+                2: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[14];
+                end
+                3:  DAC_sclk <= 0;
+                4: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[13];
+                end
+                5:  DAC_sclk <= 0;
+                6: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[12];
+                end
+                7:  DAC_sclk <= 0;
+                8: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[11];
+                end
+                9:  DAC_sclk <= 0;
+                10: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[10];
+                end
+                11: DAC_sclk <= 0;
+                12: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[9];
+                end
+                13: DAC_sclk <= 0;
+                14: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[8];
+                end
+                15: DAC_sclk <= 0;
+                16: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[7];
+                end
+                17: DAC_sclk <= 0;
+                18: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[6];
+                end
+                19: DAC_sclk <= 0;
+                20: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[5];
+                end
+                21: DAC_sclk <= 0;
+                22: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[4];
+                end
+                23: DAC_sclk <= 0;
+                24: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[3];
+                end
+                25: DAC_sclk <= 0;
+                26: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[2];
+                end
+                27: DAC_sclk <= 0;
+                28: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[1];
+                end
+                29: DAC_sclk <= 0;
+                30: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= r_set_data[0];
+                end
+                31: DAC_sclk <= 0;
+                32: begin
+                    DAC_sclk <= 1;
+                    DAC_din  <= 0;
+                end
+                33: begin
+                    DAC_sclk <= 1;
+                    DAC_cs_n <= 1;
+                end
+                default: begin
+                    DAC_sclk <= DAC_sclk;
+                    DAC_din  <= DAC_din;
+                    DAC_cs_n <= DAC_cs_n;
+                end
+            endcase
+        end
+        else begin
+            DAC_sclk <= DAC_sclk;
+            DAC_din  <= DAC_din;
+            DAC_cs_n <= DAC_cs_n;
+        end
+    end
+
 
 endmodule
--- src/tlv5618_driver_tb.v
+++ src/tlv5618_driver_tb.v
@@ -1,10 +1,123 @@
 // tlv5618_driver_tb.v
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 module tlv5618_driver_tb;
-    reg clk = 1;
-    reg rst_n;
-    
-    always #5 clk = ~clk;
+    reg         clk = 1;
+    reg         rst_n;
+    // to user
+    reg  [15:0] set_data;
+    reg         set_go;
+    wire        set_done;
+
+    //to DAC
+    wire        DAC_cs_n;
+    wire        DAC_sclk;
+    wire        DAC_din;
+
+    reg [15:0] data;
+
+    always #10 clk = ~clk;
+
+    initial begin
+        rst_n<=0;
+        set_go<=0;
+        set_data<=0;
+        #100;
+        rst_n=1;
+        #50;
+        //第一次传输
+        set_go=1;
+        set_data= 16'b0101_0111_1101_0000;
+        #20; 
+        set_go=0;
+        wait(~DAC_cs_n);
+        @(negedge DAC_sclk);
+        data[15]=DAC_din;
+        @(negedge DAC_sclk);
+        data[14]=DAC_din;
+        @(negedge DAC_sclk);
+        data[13]=DAC_din;
+        @(negedge DAC_sclk);
+        data[12]=DAC_din;
+        @(negedge DAC_sclk);
+        data[11]=DAC_din;
+        @(negedge DAC_sclk);
+        data[10]=DAC_din;
+        @(negedge DAC_sclk);
+        data[9]=DAC_din;
+        @(negedge DAC_sclk);
+        data[8]=DAC_din;
+        @(negedge DAC_sclk);
+        data[7]=DAC_din;
+        @(negedge DAC_sclk);
+        data[6]=DAC_din;
+        @(negedge DAC_sclk);
+        data[5]=DAC_din;
+        @(negedge DAC_sclk);
+        data[4]=DAC_din;
+        @(negedge DAC_sclk);
+        data[3]=DAC_din;
+        @(negedge DAC_sclk);
+        data[2]=DAC_din;
+        @(negedge DAC_sclk);
+        data[1]=DAC_din;
+        @(negedge DAC_sclk);
+        data[0]=DAC_din;
+        #200;
+
+        //第二次传输
+        set_go=1;
+        set_data= 16'b1100_0011_1110_1000;
+        #20; 
+        set_go=0;
+        wait(~DAC_cs_n);
+        @(negedge DAC_sclk);
+        data[15]=DAC_din;
+        @(negedge DAC_sclk);
+        data[14]=DAC_din;
+        @(negedge DAC_sclk);
+        data[13]=DAC_din;
+        @(negedge DAC_sclk);
+        data[12]=DAC_din;
+        @(negedge DAC_sclk);
+        data[11]=DAC_din;
+        @(negedge DAC_sclk);
+        data[10]=DAC_din;
+        @(negedge DAC_sclk);
+        data[9]=DAC_din;
+        @(negedge DAC_sclk);
+        data[8]=DAC_din;
+        @(negedge DAC_sclk);
+        data[7]=DAC_din;
+        @(negedge DAC_sclk);
+        data[6]=DAC_din;
+        @(negedge DAC_sclk);
+        data[5]=DAC_din;
+        @(negedge DAC_sclk);
+        data[4]=DAC_din;
+        @(negedge DAC_sclk);
+        data[3]=DAC_din;
+        @(negedge DAC_sclk);
+        data[2]=DAC_din;
+        @(negedge DAC_sclk);
+        data[1]=DAC_din;
+        @(negedge DAC_sclk);
+        data[0]=DAC_din;
+        #200;
+
+        $finish;
+    end
+
+    tlv5618_driver u_tlv5618_driver (
+        .clk     (clk),
+        .rst_n   (rst_n),
+        .set_data(set_data),
+        .set_go  (set_go),
+        .set_done(set_done),
+        .DAC_cs_n(DAC_cs_n),
+        .DAC_sclk(DAC_sclk),
+        .DAC_din (DAC_din)
+    );
+
 
     initial begin
         $dumpfile("wave.vcd");
